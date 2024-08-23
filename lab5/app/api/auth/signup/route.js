4fe1453c41bda8afe710b2157361591fae680521
alextann <hey22675@gmail.com>
2024-08-23 15:08:27 +0700

    init
    test init

---
+++ lab5/app/api/auth/signup/route.js
@@ -0,0 +1,29 @@
+import { Prisma, PrismaClient } from "@prisma/client";
+import {hash,hashSync} from "bcrypt";
+import {hash,hashSync} from "bcrypt"
+
+const prisma = new PrismaClient();
+
+export async function POST(req) {
+ try{   const {email,password,name} = await req.json();
+    //const hashedPassword = await hash (password,10);
+    const hashedPassword = hashSync (password,10);
+
+
+    const newUser = await prisma.user.create({
+        data:{
+            email,
+            name,
+            password: hashedPassword,
+        } ,
+    });
+
+    return Response.json({
+        msg: "Use Created !!",
+        newUser,
+
+    });
+}catch(err){
+        return Response.json(err,{ status : 500 });
+    }
+}
---
+++ lab5/app/compornents/SessionProvider.js
@@ -0,0 +1,4 @@
+'use client'
+
+import {SessiomProvier } from "next-auth";
+export default SessiomProvier;
